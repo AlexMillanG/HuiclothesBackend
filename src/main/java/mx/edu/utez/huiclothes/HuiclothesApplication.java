9a3f25008aacea834f7101d7163106c66d573b5e
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2024-09-09 18:06:03 -0600

    primer commit

---
+++ src/main/java/mx/edu/utez/huiclothes/HuiclothesApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.huiclothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuiclothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuiclothesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/utez/huiclothes/HuiclothesApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.huiclothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuiclothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
