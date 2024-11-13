59dbcbc85661ecf8c3e1c35321b28b416f4e5e80
NONAMEB-312 <133677796+NONAMEB-312@users.noreply.github.com>
2024-11-13 17:46:05 -0600

    addressService listo

--- src/main/java/mx/edu/utez/huiclothes/models/address/AddressRepository.java
+++ src/main/java/mx/edu/utez/huiclothes/models/address/AddressRepository.java
@@ -3,12 +3,11 @@ package mx.edu.utez.huiclothes.models.address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface AddressRepository extends JpaRepository<AddressBean, Long> {
-    Optional<AddressBean> findById(Long id);
-    //Optional<AddressBean> findByCountry(String country);
-    //Optional<AddressBean> findByState(String state);
-    //Optional<AddressBean> findByZipcode(String zipcode);
+    List<AddressBean> findByCountry(String country);
+    List<AddressBean> findByState(String state);
+    List<AddressBean> findByZipCode(String zipCode);
 }
